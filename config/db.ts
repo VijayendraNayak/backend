578037f7c16c1643ad9c135e219593aecf34f598
unknown <vijayendranayak19@gmail.com>
2025-01-03 13:47:40 +0530

    commit

--- config/db.ts
+++ config/db.ts
@@ -1,11 +1,20 @@
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
-const connectDb=async()=>{
-    try{
-        await mongoose.connect("mongodb://localhost:27017/finedine");
-        console.log("Database connected Successfully!!!");
-    }catch(err){
-        console.log(err);
-    }
-}
+const connectDb = async () => {
+  const mongoUri = process.env.MONGO_URI as string; // Type assertion
+
+  if (!mongoUri) {
+    throw new Error("MONGO_URI is not defined in the environment variables");
+  }
+
+  try {
+    await mongoose.connect(mongoUri);
+    console.log("Database connected successfully!!!");
+  } catch (err) {
+    console.error(`Error connecting to MongoDB:${err}`);
+  }
+};
+
 export default connectDb;
--- server.ts
+++ server.ts
@@ -17,5 +17,5 @@ connectDb();
 app.use(express.json());
 app.use("/api", reserveRouter);
 
-const PORT = 5000;
+const PORT = 7000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
